5382214c8b2ecc1d3337f1b4ceb5946bc05afd47
sodun2456 <baesoeun1004@gmail.com>
2025-10-02 01:05:19 +0900

    첫번째 커밋

---
+++ firstTkinter.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+root=tk.Tk()
+root.title("Tkinter예제")
+root.geometry("200x100")
+
+
+label=tk.Label(root, text="Hello, Tkinter!")
+label.pack(pady=20)
+
+
+root.mainloop()
